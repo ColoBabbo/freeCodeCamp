8af74c729ae66ef55cb11510be05a81076067391
ColoBabbo <navessolutions@gmail.com>
2024-08-14 09:31:17 -0600

    begin Budget App Project

---
+++ scientificComputingWithPython/budgetAppProject.py
@@ -0,0 +1,84 @@
+class Category:
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+
+    def __repr__(self):
+        return f'Category: {self.category}\nBalance: {str(self.get_balance())}\nEntries: {len(self.ledger)}'
+
+    def __str__(self):
+        # if sum(ledger[]):
+            # return self.ledger
+        # return f'Category: {self.category}\nBalance: {str(self.get_balance())}\nEntries: {len(self.ledger)}'
+        return self.print_cat()
+
+    def print_cat(self):
+        line_width = 30
+        output = ''
+
+        # print(f"{self.category}".center(line_width,'*'))
+        output += f"{self.category}".center(line_width,'*')
+        output +='\n'
+        for entry in self.ledger:
+            spaces = len(f"{entry['amount']:.2f}")
+            # print(
+            #     f"{entry['description'][:29-spaces]:<{line_width - spaces}}|{entry['amount']:>{spaces}.2f}"
+            #     f""
+            #     )
+            output += (
+                f"{entry['description'][:29-spaces]:<{line_width - spaces}}{entry['amount']:>{spaces}.2f}"
+                f"\n"
+                )
+        # print(f"Total: {self.get_balance()}")
+        output += f"Total: {self.get_balance()}"
+        # print(output)   
+        return output
+
+
+    def deposit(self, amount, description=''):
+        self.ledger.append({'amount': amount, 'description': description})
+        pass
+
+    def withdraw(self, amount, description=''):
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -amount, 'description': description})
+            return True
+        return False
+
+    def get_balance(self):
+        balance = 0
+        for entry in self.ledger:
+            balance += entry['amount']
+        # print(balance)
+        return balance
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount):
+            self.withdraw(amount, f'Transfer to {category.category}')
+            category.deposit(amount, f'Transfer from {self.category}')
+            # print(category)
+            return True
+        return False
+
+    def check_funds(self, amount):
+        if amount > self.get_balance():
+            return False
+        return True
+
+def create_spend_chart(categories):
+    pass
+
+
+food = Category('Food')
+food.deposit(1000, 'deposit')
+# print(food)
+# food.get_balance()
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert')
+clothing = Category('Clothing')
+food.transfer(50, clothing)
+food.print_cat()
+print(food)
+# print(food.ledger)
+# print(clothing)
+
